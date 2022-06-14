802909dd9fc5f9a30c4d08a3aaba0fbb7670c279
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2022-06-14 21:54:36 +0200

    display list of attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :event_attendance, foreign_key: :attended_event_id 
+  has_many :attendees, through: :event_attendances, foreign_key: :attended_event_id 
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :date, presence: true
